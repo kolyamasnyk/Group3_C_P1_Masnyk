5debaf1d5b6d37cfa08a1f8309e9d1bc82d446e6
kolyamasnyk <kolya.masnyk@gmail.com>
2023-06-05 00:20:51 +0300

    upd HW1

---
+++ Masnyk_HW_1/HW1.cs
@@ -0,0 +1,14 @@
+﻿namespace Masnyk_HW_1
+{
+    internal class HW1
+    {
+        static void Main(string[] args)
+        {
+            string name = "Mykola";
+            string surname = "Masnyk";
+            int age = 33;
+
+            Console.WriteLine($"Менi- {age} , Мене звати {name} {surname}");
+        }
+    }
+}
