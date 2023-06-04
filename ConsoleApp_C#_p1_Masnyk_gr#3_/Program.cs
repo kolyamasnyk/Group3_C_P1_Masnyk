066ef127106c312ef286d864da30eea8b1561561
Mykola.Masnyk <Mykola.Masnyk@MMASNYK-N>
2023-06-04 19:28:36 +0300

    Add project files.

---
+++ ConsoleApp_C#_p1_Masnyk_gr#3_/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MykolaMasnyk_gr3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Mykola";
+            string surname = "Masnyk";
+            int age = 33;
+
+            Console.WriteLine($"Менi- {age}, Мене звати {name} {surname}");
+        }
+    }
+}   
