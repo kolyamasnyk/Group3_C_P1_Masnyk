aad1d983dfe23efbeef093934b1497f74db03039
kolyamasnyk <kolya.masnyk@gmail.com>
2023-06-05 22:21:42 +0300

    HW3_2

--- Masnyk_HW_3/HW3.cs
+++ Masnyk_HW_3/HW3.cs
@@ -11,11 +11,11 @@
             Console.WriteLine("Введiть друге число:");
             int number2 = Convert.ToInt32(Console.ReadLine());
 
-            if (number1 <0 && number2>0)
-             {
+            if (number1 < 0 && number2 > 0)
+            {
                 Console.WriteLine("True, одне число вiдємне одне додатне");
-             }
-            else if (number1 >0 && number2<0)
+            }
+            else if (number1 > 0 && number2 < 0)
             {
                 Console.WriteLine("True, одне число вiдємне одне додатне");
             }
@@ -35,6 +35,27 @@
             {
                 Console.WriteLine("Ви вели не коректнi занчення"); // літери чи спец символи водити не можна бо по умові інпути є int, але на всякий випадок.
             }
+
+            // Write a C# program to check the sum of the two given integers and return true if one of the integer is 20 or if their sum is 20
+            Console.WriteLine();
+            Console.WriteLine("Введiть число1:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введiть число2:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int Summ = num1+num2;
+            if (num1 ==20 || num2 ==20)
+            {
+                Console.WriteLine("True, одне з чисел є 20");
+            }
+            else if (Summ==20)
+            {
+                Console.WriteLine("True, Сума чисел є 20");
+            }
+            else
+            {
+                Console.WriteLine("False, Сума, а також жодне із чисел не є 20");
+            }
+
         }
-        }
+    }
     }
